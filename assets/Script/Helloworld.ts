1fec01326e71cfcade275aa77a34a52505eae037
hwh97 <974815768@qq.com>
2020-07-07 16:25:44 +0800

    initial commit

---
+++ assets/Script/Helloworld.ts
@@ -0,0 +1,45 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property(cc.Button)
+    playBtn: cc.Button = null;
+
+    @property
+    text: string = '';
+
+    @property
+    aa: string = "";
+
+    start () {
+        window.addEventListener("flutterInAppBrowserPlatformReady", function(event) {
+        });
+        let _this = this;
+        window.appSendJs = function(args){
+            console.log(args)
+            _this.label.string = args;
+        }
+        // init logic
+        this.label.string = this.text;
+        this.playBtn.node.on('click', this.playVideo, this);
+    }
+
+    playVideo () {
+        try {
+            const data = JSON.stringify([1, true, ['bar', 5], {foo: 'baz'}]);
+            if (window.flutter_inappwebview.callHandler) {
+                window.flutter_inappwebview.callHandler('handlerName', data).then(function(result) {
+                    console.log(result);
+                });
+            } else {
+                window.flutter_inappwebview._callHandler('handlerName', setTimeout(function(){}), JSON.stringify(data));
+            }
+        } catch(e) {
+            console.log(e)
+        }
+    }
+}
